6e7652537c2f074d095a0f82a28d3f9d94601d09
SepehrRajabi <50242165+SepehrRajabi@users.noreply.github.com>
2021-08-25 18:31:44 +0430

    added "write_at_center" for pirnting stuff on the monitor and some functions to get info about the reactor

--- reactor.lua
+++ reactor.lua
@@ -1,22 +1,50 @@
 os.loadAPI("button.lua")
+
+-- setting the monitor handler and getting its size
 monitor_handler = peripheral.wrap("right")
- 
+width, height = monitor_handler.getSize()
+
+-- reactor peripheral wrapper
+reactor = peripheral.wrap("ic2:reactor_chamber_0")
+core = reactor.getReactorCore()
+curr_power_output = core.getOfferedEnergy()
+reactor_info = core.getMetadata()
+
 button.setMonitor(monitor_handler)
 startbutton = button.create().setSize(5, 5).setColor(colors.blue)
 startbutton.setPos(2, 2)
 startbutton.setText("start")
- 
-local status = true -- which means a rs signal will be sent
-startbutton.onClick(function() rs.setOutput("left", status) status = not status end)
- 
+
+-- the start button functionality
+local isOn = true -- which means a rs signal will be sent
+startbutton.onClick(function() rs.setOutput("left", isOn) isOn = not isOn end)
+
+-- getting the reactor heat percentage
+function get_heat_perc()
+    max_heat = info.reactor.maxHeat
+    curr_heat = info.reactor.heat
+    perc = (curr_heat / max_heat) * 100
+    return perc
+end
+
+function write_at_center(text, line)
+    monitor_handler.setBackgroundColor(colors.black)
+    monitor_handler.setTextColor(colors.blue)
+    text_len = (string.len(text))
+    diff = math.floor(width - text_len)
+    text_x = math.floor(diff / 2)
+    monitor_handler.setCursorPos(text_x + 1, line)
+    monitor_handler.write(text)
+end
+
 function draw_bar()
-    bar = math.floor(5)
-    monitor_handler.setCursorPos(2, 8)
-    monitor_handler.setBackgroundColor(colors.blue)
-    monitor_handler.write(string.rep("  ", 9))
+    bar = curr_power_output
+    monitor_handler.setCursorPos(2, 4)
+    monitor_handler.setBackgroundColor(colors.red)
+    monitor_handler.write(string.rep(" ", bar))
 end
 
 while true do
     button.await(startbutton)
-    draw_bar()
+    write_at_center(" Nuclear Reator 0 ", 1)
 end
