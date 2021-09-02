6a606ae574f6122687930e4fcfbf3d6001c91b10
SepehrRajabi <50242165+SepehrRajabi@users.noreply.github.com>
2021-09-02 19:08:37 +0430

    completed draw_bar_heat() function to show the heat percentage on the screen using a bar

--- reactor.lua
+++ reactor.lua
@@ -20,13 +20,14 @@ local isOn = true -- which means a rs signal will be sent
 startbutton.onClick(function() rs.setOutput("left", isOn) isOn = not isOn end)
 
 -- getting the reactor heat percentage
-function get_heat_perc()
-    max_heat = info.reactor.maxHeat
-    curr_heat = info.reactor.heat
+function get_heat_info()
+    max_heat = reactor_info.reactor.maxHeat
+    curr_heat = reactor_info.reactor.heat
     perc = (curr_heat / max_heat) * 100
-    return perc
+    return max_heat, curr_heat, perc
 end
 
+-- to write a text at the center of the screen at a given line
 function write_at_center(text, line)
     monitor_handler.setBackgroundColor(colors.black)
     monitor_handler.setTextColor(colors.blue)
@@ -37,14 +38,22 @@ function write_at_center(text, line)
     monitor_handler.write(text)
 end
 
-function draw_bar()
-    bar = curr_power_output
-    monitor_handler.setCursorPos(2, 4)
+function draw_bar_heat()
+    max_heat, curr_heat, perc = get_heat_info()
+    bar = math.floor(((curr_heat / max_heat) * (width - 2)) + 0.5)
+    -- drawing the blue bar
+    monitor_handler.setPos(2, height - 4)
+    monitor_handler.setBackgroundColor(colors.blue)
+    monitor_handler.write(string.rep(" ", width - 2))
+    -- drawing the percentage bar on top of the full blue bar
+    monitor_handler.setCursorPos(2, height - 4)
     monitor_handler.setBackgroundColor(colors.red)
     monitor_handler.write(string.rep(" ", bar))
 end
 
+-- main loop
 while true do
     button.await(startbutton)
     write_at_center(" Nuclear Reator 0 ", 1)
+    draw_bar_heat()
 end
