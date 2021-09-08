eec29253f7d2a3d11249cece67505e832d060adb
SepehrRajabi <50242165+SepehrRajabi@users.noreply.github.com>
2021-09-09 03:09:48 +0430

    Displaying some info about the power output and the reactor core heat. Also made everything look a little bit nicer.

--- reactor.lua
+++ reactor.lua
@@ -1,7 +1,8 @@
 os.loadAPI("button.lua")
 
 -- setting the monitor handler and getting its size
-monitor_handler = peripheral.wrap("right")
+monitor_side = "right"
+monitor_handler = peripheral.wrap(monitor_side)
 width, height = monitor_handler.getSize()
 
 -- reactor peripheral wrapper
@@ -10,6 +11,7 @@ core = reactor.getReactorCore()
 curr_power_output = core.getOfferedEnergy()
 reactor_info = core.getMetadata()
 
+-- setting up the button using the button api
 button.setMonitor(monitor_handler)
 startbutton = button.create().setSize(5, 5).setColor(colors.blue)
 startbutton.setPos(2, 2)
@@ -17,7 +19,11 @@ startbutton.setText("start")
 
 -- the start button functionality
 local isOn = true -- which means a rs signal will be sent
-startbutton.onClick(function() rs.setOutput("left", isOn) isOn = not isOn end)
+function start_btn()
+    rs.setOutput("left", isOn)
+    isOn = not isOn
+end
+startbutton.onClick(start_btn())
 
 -- getting the reactor heat percentage
 function get_heat_info()
@@ -28,9 +34,10 @@ function get_heat_info()
 end
 
 -- to write a text at the center of the screen at a given line
-function write_at_center(text, line)
-    monitor_handler.setBackgroundColor(colors.black)
-    monitor_handler.setTextColor(colors.blue)
+function write_at_center(text, line, color)
+    monitor_handler.setBackgroundColor(colors.black) -- setting the screen color to black
+    monitor_handler.setTextColor(color) -- setting the color of the text we want to write
+    -- centering the cusrsor position based on the length of the text
     text_len = (string.len(text))
     diff = math.floor(width - text_len)
     text_x = math.floor(diff / 2)
@@ -40,7 +47,7 @@ end
 
 function draw_bar_heat()
     max_heat, curr_heat, perc = get_heat_info()
-    bar = math.floor(((curr_heat / max_heat) * (width - 2)) + 0.5)
+    heat_bar = math.floor(((curr_heat / max_heat) * (width - 2)) + 0.5)
     -- drawing the blue bar
     monitor_handler.setPos(2, height - 4)
     monitor_handler.setBackgroundColor(colors.blue)
@@ -48,12 +55,18 @@ function draw_bar_heat()
     -- drawing the percentage bar on top of the full blue bar
     monitor_handler.setCursorPos(2, height - 4)
     monitor_handler.setBackgroundColor(colors.red)
-    monitor_handler.write(string.rep(" ", bar))
+    monitor_handler.write(string.rep(" ", heat_bar))
+end
+
+function draw_energy_bar()
+
 end
 
 -- main loop
 while true do
     button.await(startbutton)
-    write_at_center(" Nuclear Reator 0 ", 1)
+    write_at_center(" Nuclear Reator 0 ", 1, colors.red)
+    write_at_center("Offered Energy: " .. core.getOfferedEnergy() .. " EU", 5, colors.orange)
+    write_at_center("Core Heat Status", height - 5, colors.cyan)
     draw_bar_heat()
 end
